f4a692a7c901c90cc6dccdc66e05cc901f0ddd8d
Jc <jc.florendo696@gmail.com>
2020-02-12 14:30:09 +0800

    Added class in header image.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 		<div class="row">
 			<div class="col-12 text-center">
 				<!-- <h1>Experience Japan!</h1> -->
-				<img src="https://i.imgur.com/BcB01XZ.png" width="700">
+				<img src="https://i.imgur.com/BcB01XZ.png" class="img-fluid" width="700">
 			</div>
 		</div>
 
